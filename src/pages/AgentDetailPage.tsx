df1e5c5c89b97c4c82e288cca6f4aebe8a9624f4
Tony-Doby <swhermespc@gmail.com>
2026-05-29 17:37:13 +0700

    style: use flex to bring columns closer together

--- src/pages/AgentDetailPage.tsx
+++ src/pages/AgentDetailPage.tsx
@@ -188,7 +188,7 @@ export default function AgentDetailPage() {
 
         <div className="p-5">
           {mainTab === 'info' && (
-            <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 justify-start">
+            <div className="flex flex-col lg:flex-row gap-x-4 gap-y-2">
               {/* Left column */}
               <div className="space-y-2">
                 <InfoRow label="Tên" value={agent.full_name} />
